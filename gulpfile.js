86de0bc424588fd1242cfb41a4b03743e0cf0fc2
alina_ganich <alina.ganich@icloud.com>
2019-06-23 20:17:13 +0300

    sarajevo

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+//Подключение 
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+const less = require('gulp-less');
+
+
+
+const lessFiles = [
+    './src/less/main.less',
+    './src/less/_banner.less',
+    './src/less/media.less'
+]
+const jsFiles = [
+        './src/js/lib.js',
+        './src/js/main.js'
+    ]
+    //Таск на стили CSS
+function styles() {
+    return gulp.src(lessFiles)
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(concat('style.css'))
+
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./build/css'))
+        .pipe(browserSync.stream());
+}
+
+//Таск на JS
+function scripts() {
+    return gulp.src(jsFiles)
+        .pipe(concat('script.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/js'))
+        .pipe(browserSync.stream());
+
+}
+
+function clean() {
+    return del(['build/*'])
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    // gulp.watch('./src/css/**/*.css', styles)
+    gulp.watch('./src/less/**/*.less', styles)
+    gulp.watch('./src/js/**/*.js', scripts)
+    gulp.watch("./*.html").on('change', browserSync.reload);
+}
+
+gulp.task('styles', styles);
+gulp.task('scripts', scripts);
+
+gulp.task('del', clean);
+gulp.task('watch', watch);
+gulp.task('build', gulp.series(clean, gulp.parallel(styles, scripts)));
+gulp.task('dev', gulp.series('build', 'watch'));
